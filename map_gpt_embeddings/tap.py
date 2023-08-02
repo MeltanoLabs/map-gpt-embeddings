2155fec353868ef7b6334a8c88a3895130440272
pnadolny13 <patnadolny@gmail.com>
2023-08-02 17:36:49 -0400

    rename tap

--- map_gpt_embeddings/tap.py
+++ map_gpt_embeddings/tap.py
@@ -3,6 +3,6 @@ from singer_sdk.tap_base import Tap
 class TapOpenAI(Tap):
     """OpenAI tap class."""
 
-    name = "tap-openai"
+    name = "map-gpt-embeddings-tap-openai"
     def discover_streams(self):
         return []
